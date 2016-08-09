748db3ef3c328bf6d80bfed0ded6add0c786bec6
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-08-08 22:56:27 -0400

    Removed a debug log

--- include/D_DebugDrawer2D.h
+++ include/D_DebugDrawer2D.h
@@ -34,10 +34,6 @@ namespace Diamond {
         void draw(const PointList2D &poly, const RGBA &color);
 
         void draw(const PolyCollider *poly, const RGBA &color) {
-            // DEBUG
-            for (auto point : poly->worldPoints())
-                std::cout << point << std::endl;
-
             draw(poly->worldPoints(), color);
         }
         
