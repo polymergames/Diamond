9e424ee1b176d78a11c5f88631a00a32f0318797
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-08-23 11:04:42 -0400

    Removed Behavior stuff from Entity

--- include/D_Entity.h
+++ include/D_Entity.h
@@ -18,13 +18,11 @@
 #define D_ENTITY_H
 
 #include <map>
-#include "D_Behavior.h"
 #include "D_Component.h"
 
 namespace Diamond {
     /**
-     Holds and updates a table of components indexed by type,
-     and behaviors that are destroyed when they expire.
+     Holds and updates a table of components indexed by type.
      The purpose of this class is to handle component ownership.
      Therefore, it takes ownership of any component added to it
      and destroys all of its components at the end of its lifetime.
@@ -39,13 +37,10 @@ namespace Diamond {
         Entity& operator=(const Entity&) = delete;
 
         // But moving is OK
-        Entity(Entity &&other) : m_components(std::move(other.m_components)),
-                                 m_behaviors (std::move(other.m_behaviors)){}
-        
+        Entity(Entity &&other) : m_components(std::move(other.m_components)) {}
         Entity& operator=(Entity &&other) {
             if (this != &other) {
                 m_components = std::move(other.m_components);
-                m_behaviors  = std::move(other.m_behaviors);
             }
             return *this;
         }
@@ -54,36 +49,16 @@ namespace Diamond {
         /**
          Call this once a frame to update all of this entity's components.
         */
-        void updateComponents(tD_delta delta) {
+        void updateComponents(tD_delta delta) const {
             for (auto it = m_components.begin(); it != m_components.end(); ++it)
                 it->second->update(delta);
         }
 
-        /**
-         Call this after physics updates.
-        */
-        void postPhysicsUpdateComponents(tD_delta delta) {
+        void postPhysicsUpdateComponents(tD_delta delta) const {
             for (auto it = m_components.begin(); it != m_components.end(); ++it)
                 it->second->postPhysicsUpdate(delta);
         }
 
-        
-        /**
-         Call this once a frame to update all of this entity's behaviors.
-        */
-        void updateBehaviors(tD_delta delta) {
-            for (auto be : m_behaviors)
-                be->update(delta);
-        }
-        
-        /**
-         Call this after physics updates.
-        */
-        void postPhysicsUpdateBehaviors(tD_delta delta) {
-            for (auto be : m_behaviors)
-                be->postPhysicsUpdate(delta);
-        }
-        
 
         void addComponent(const std::string &name, Component &component) { 
             addComponent(name, &component);
@@ -93,12 +68,11 @@ namespace Diamond {
             addComponent(name, SharedPtr<Component>(component));
         }
 
-        void addComponent(const std::string &name,
-                          const SharedPtr<Component> &component) {
+        void addComponent(const std::string &name, SharedPtr<Component> component) {
             m_components[name] = component;
         }
-        
 
+        // TODO: make const get functions return const pointers!
         SharedPtr<Component> getComponent(const std::string &name) const {
             auto i = m_components.find(name);
             if (i != m_components.end())
@@ -106,44 +80,17 @@ namespace Diamond {
             else
                 return nullptr;
         }
-        
-// TODO: fix these and make the other get functions return const pointers
-//        SharedPtr<Component> getComponent(const std::string &name) {
-//            return std::const_pointer_cast<Component>(getComponent(name));
-//        }
-        
 
         template <class T>
         SharedPtr<T> getComponent(const std::string &name) const {
             return std::dynamic_pointer_cast<T>(getComponent(name));
         }
-        
-//        template <class T>
-//        SharedPtr<T> getComponent(const std::string &name) {
-//            return std::const_pointer_cast<T>(getComponent<T>(name));
-//        }
-        
 
         void removeComponent(const std::string &name) {
             m_components.erase(name);
         }
-        
-        
-        
-        void addBehavior(Behavior &behavior) {
-            addBehavior(&behavior);
-        }
-        
-        void addBehavior(Behavior *behavior) {
-            addBehavior(SharedPtr<Behavior>(behavior));
-        }
-        
-        void addBehavior(const SharedPtr<Behavior> &behavior) {
-            m_behaviors.push_back(behavior);
-        }
 
     protected:
-        std::vector<SharedPtr<Behavior> >            m_behaviors;
         std::map<std::string, SharedPtr<Component> > m_components;
     };
 }
