367617ef8c672b2cbb1d169d6d924d57e1e7eb47
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2017-04-11 13:30:59 -0400

    Created UIButton class header

---
+++ include/D_UIButton.h
@@ -0,0 +1,63 @@
+/*
+    Copyright 2017 Ahnaf Siddiqui
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef D_UI_BUTTON_H
+#define D_UI_BUTTON_H
+
+#include <functional>
+#include "D_UIView.h"
+
+namespace Diamond {
+    using UITouchCallback = std::function<void(const Vector2<tD_pos>&)>;
+    
+    class UIButton : public UIView {
+    public:
+        UIButton(DTransform2 transform,
+                 tD_pos width,
+                 tD_pos height,
+                 const UITouchCallback &onTouchDown = nullptr,
+                 const UITouchCallback &onTouchDrag = nullptr,
+                 const UITouchCallback &onTouchUp = nullptr);
+        
+        UIButton(const UITouchCallback &onTouchDown = nullptr,
+                 const UITouchCallback &onTouchDrag = nullptr,
+                 const UITouchCallback &onTouchUp = nullptr);
+        
+        virtual ~UIButton() {}
+        
+        
+        void setOnTouchDown(const UITouchCallback &callback)
+        { onTouchDown = callback; }
+        
+        void setOnTouchDrag(const UITouchCallback &callback)
+        { onTouchDrag = callback; }
+        
+        void setOnTouchUp(const UITouchCallback &callback)
+        { onTouchUp = callback; }
+        
+        
+        void handleTouchDown(const Vector2<tD_pos> &touchPos) override;
+        void handleTouchDrag(const Vector2<tD_pos> &touchPos) override;
+        void handleTouchUp(const Vector2<tD_pos> &touchPos) override;
+        
+    private:
+        UITouchCallback onTouchDown;
+        UITouchCallback onTouchDrag;
+        UITouchCallback onTouchUp;
+    };
+}
+
+#endif // D_UI_BUTTON_H
